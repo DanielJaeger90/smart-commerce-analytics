fc078d13a58af88c9f66e8dc6fc602a588763470
daniel.iturralde.26@gmail.com <daniel.iturralde.26@gmail.com>
2026-07-20 19:29:19 +0200

    Crear estructura inicial de la aplicación

--- componente_datos.py
+++ componente_datos.py
@@ -11,17 +11,9 @@ class IngestorDatos:
     def cargar_datos(self, archivo) -> pd.DataFrame:
         """Carga los datos desde un archivo CSV y realiza la limpieza inicial."""
         try:
-            self.datos = pd.read_csv(self.ruta_archivo)
-            self.limpiar_datos()
-            return self.datos
+            df = pd.read_csv(archivo, sep=None, engine='python', encoding='utf-8-sig') # Detecta automáticamente el separador
+            df = self.limpiar_datos(df)
+            return df
         except Exception as e:
             print(f"Error al cargar los datos: {e}")
-            return None
-
-    def limpiar_datos(self):
-        """
-            Realiza la limpieza de los datos, eliminando valores nulos y duplicados.
-        """
-        if self.datos is not None:
-            self.datos.dropna(inplace=True)
-            self.datos.drop_duplicates(inplace=True)
+            return pd.DataFrame()  # Retorna un DataFrame vacío en caso de error
